defb6bb660f08f6b49b1374a79d3a740d5ad150c
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2022-12-03 15:06:03 +0530

    Adding the doubly linked list with push, pop, shift, unshift, get and print methods

---
+++ doubly-linked-list/index.ts
@@ -0,0 +1,172 @@
+/*
+    Problem Statement:
+    Implement a doubly linked list
+*/
+
+class ListNode {
+  val: number | string;
+  next: ListNode | null;
+  prev: ListNode | null;
+
+  constructor(val: number | string) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  head: ListNode | null;
+  tail: ListNode | null;
+  length: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  /* Add a new node to the end of the list  */
+  push(val: number | string) {
+    const newNode = new ListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  /* Removing a node from the end of the list */
+  pop() {
+    if (!this.head) return;
+
+    let removedNode: ListNode | null = null;
+    if (this.length === 1) {
+      removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      const prevNode = this.tail!.prev;
+
+      removedNode = this.tail;
+      removedNode!.prev = null;
+
+      prevNode!.next = null;
+      this.tail = prevNode;
+    }
+
+    this.length -= 1;
+    return removedNode;
+  }
+
+  /* Remove a node from the beginning of the list */
+  shift() {
+    if (!this.head) return;
+
+    let removedNode: ListNode | null = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      const nextNode = removedNode.next;
+      this.head = nextNode;
+      nextNode!.prev = null;
+      removedNode.next = null;
+    }
+
+    this.length -= 1;
+    return removedNode;
+  }
+
+  /* Add a node to the beginning of the list */
+  unshift(val: number | string) {
+    const newNode = new ListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  /* Get the node at a given position/index */
+  get(index: number) {
+    if (!this.head) return;
+
+    if (index < 0 || index >= this.length) return;
+
+    const startFromHead = index <= Math.floor(this.length / 2);
+
+    if (startFromHead) {
+      let currentNode: ListNode | null = this.head;
+      let counter = 0;
+      while (currentNode) {
+        if (counter === index) return currentNode;
+        currentNode = currentNode.next;
+        counter += 1;
+      }
+    } else {
+      let currentNode: ListNode | null = this.tail;
+      let counter = this.length - 1;
+      while (currentNode) {
+        if (counter === index) return currentNode;
+        currentNode = currentNode.prev;
+        counter -= 1;
+      }
+    }
+  }
+
+  print() {
+    let currentNode = this.head;
+    let list = "";
+    while (currentNode) {
+      list += `${currentNode.val} -> `;
+      currentNode = currentNode.next;
+    }
+    return list + " null";
+  }
+}
+
+// Test cases
+const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.push(6);
+list.push(7);
+list.push(8);
+list.push(9);
+list.push(10);
+// console.log(list.push(5));
+// console.log(list.pop());
+// console.log(list.print());
+// console.log(list.shift());
+// console.log(list.print());
+// console.log(list);
+// console.log(list.unshift(111));
+console.log(list.print());
+// console.log("get: ", list.get(0));
+// console.log("get: ", list.get(1));
+// console.log("get: ", list.get(2));
+// console.log("get: ", list.get(3));
+// console.log("get: ", list.get(4));
+// console.log("get: ", list.get(5));
+// console.log("get: ", list.get(6));
+// console.log("get: ", list.get(7));
+// console.log("get: ", list.get(8));
+// console.log("get: ", list.get(9));
