3a0ab8071c636e3781158838791a0f17f3ac8f3f
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2022-12-11 11:03:25 +0530

    adding the extract max method to the max binary heap implementation

--- max-binary-heap/index.ts
+++ max-binary-heap/index.ts
@@ -29,10 +29,50 @@ class MaxBinaryHeap {
     this.#bubbleUp(index);
   }
 
+  #bubbleDown(index: number) {
+    if (index >= this.#values.length - 1) return;
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = leftChildIndex + 1;
+
+    const parentValue = this.#values[index];
+    const leftChildValue = this.#values[leftChildIndex];
+    const rightChildValue = this.#values[rightChildIndex];
+
+    // If both the child indexes are out of bounds return
+    if (
+      leftChildIndex >= this.#values.length &&
+      rightChildIndex >= this.#values.length
+    )
+      return;
+
+    // If the parent is larger than both the children return
+    if (parentValue > leftChildValue && parentValue > rightChildValue) return;
+
+    // Bubble down
+    if (
+      leftChildValue > rightChildValue ||
+      rightChildIndex >= this.#values.length
+    ) {
+      this.#swap(leftChildIndex, index);
+      this.#bubbleDown(leftChildIndex);
+    } else {
+      this.#swap(rightChildIndex, index);
+      this.#bubbleDown(rightChildIndex);
+    }
+  }
+
   insert(val: number | string) {
     this.#values.push(val);
     this.#bubbleUp(this.#values.length - 1);
     return this.#values;
+  }
+
+  extractMax() {
+    if (this.#values.length === 0) return;
+    this.#swap(0, this.#values.length - 1);
+    const extractedValue = this.#values.pop();
+    this.#bubbleDown(0);
+    return extractedValue;
   }
 }
 
@@ -45,3 +85,5 @@ console.log(maxBinaryHeap.insert(18));
 console.log(maxBinaryHeap.insert(27));
 console.log(maxBinaryHeap.insert(12));
 console.log(maxBinaryHeap.insert(55));
+console.log(maxBinaryHeap.extractMax());
+console.log(maxBinaryHeap.insert(100));
